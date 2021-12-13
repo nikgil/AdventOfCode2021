1edb427b8e64f9459b5dfe9e37660e634dc1f999
Nikita Gilevskiy <github@contactnik.com>
2021-12-12 21:34:16 -0500

    uncomment gen code

--- genDay.py
+++ genDay.py
@@ -14,32 +14,32 @@ args = parser.parse_args()
 
 title = f'Day{args.day}'
 
-# open(os.path.join(days_dir, f'{title}.txt'), 'a').close()
-#
-# with open(template_loc) as f:
-#     contents = f.read()
-#     contents = contents.replace('DayX', title).replace('super(X)', f'super({args.day})')
-#
-#     path = os.path.join(target_dir, f'{title}.java')
-#
-#     with open(path, 'w') as newFile:
-#         newFile.write(contents)
-#
-# target_index = None
-#
-# with open(main_loc, 'r') as main_file:
-#     lines = main_file.readlines()
-#
-#     for (idx, line) in enumerate(lines):
-#         if 'private static void addDays() {' in line:
-#             target_index = idx
-#             break
-#
-# if lines is not None and target_index is not None:
-#     lines.insert(target_index+1, f'        days.put({args.day}, new {title}());\n')
-#
-#     with open(main_loc, 'w') as main_file:
-#         main_file.write("".join(lines))
+open(os.path.join(days_dir, f'{title}.txt'), 'a').close()
+
+with open(template_loc) as f:
+    contents = f.read()
+    contents = contents.replace('DayX', title).replace('super(X)', f'super({args.day})')
+
+    path = os.path.join(target_dir, f'{title}.java')
+
+    with open(path, 'w') as newFile:
+        newFile.write(contents)
+
+target_index = None
+
+with open(main_loc, 'r') as main_file:
+    lines = main_file.readlines()
+
+    for (idx, line) in enumerate(lines):
+        if 'private static void addDays() {' in line:
+            target_index = idx
+            break
+
+if lines is not None and target_index is not None:
+    lines.insert(target_index+1, f'        days.put({args.day}, new {title}());\n')
+
+    with open(main_loc, 'w') as main_file:
+        main_file.write("".join(lines))
 
 md_line = f'\n| Day {args.day} | [Input {args.day}](src/main/resources/{title}.txt) | [Sol {args.day}](src/main/java/com/github/nikgil/days/{title}.java) |  |  |'
 
